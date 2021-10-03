913d976295f3220e7a1ba963f34febccf9e559b0
woojun-youha <woojun.park@youha.info>
2021-10-04 02:02:27 +0900

    Complete logout

--- hw3/src/controllers/store/reducers/user.js
+++ hw3/src/controllers/store/reducers/user.js
@@ -1,27 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { userAutoLogin, userLogin, userLogout } from "../actions/user";
+import { createSlice } from '@reduxjs/toolkit';
+import { userAutoLogin, userLogin, userLogout } from '../actions/user';
 
 const INIT_STATE = {
-    currentUser: undefined,
-    isLoggedIn: false,
+  currentUser: undefined,
+  isLoggedIn: false,
 };
 
 export default createSlice({
-    name: "users",
-    initialState: INIT_STATE,
-    reducers: {},
-    extraReducers: {
-        [userAutoLogin.fulfilled.type]: (state, action) => {
-            state.currentUser = action.user;
-            state.isLoggedIn = true;
-        },
-        [userLogin.fulfilled.type]: (state, action) => {
-            state.currentUser = action.user;
-            state.isLoggedIn = true;
-        },
-        [userLogout.fulfilled.type]: (state, action) => {
-            state.currentUser = undefined;
-            state.isLoggedIn = false;
-        },
+  name: 'users',
+  initialState: INIT_STATE,
+  reducers: {},
+  extraReducers: {
+    [userAutoLogin.fulfilled.type]: (state, action) => {
+      state.currentUser = action.payload.user;
+      state.isLoggedIn = true;
     },
+    [userLogin.fulfilled.type]: (state, action) => {
+      state.currentUser = action.payload.user;
+      state.isLoggedIn = true;
+    },
+    [userLogout.fulfilled.type]: (state, action) => {
+      console.log('logout');
+      state.currentUser = undefined;
+      state.isLoggedIn = false;
+    },
+  },
 }).reducer;
